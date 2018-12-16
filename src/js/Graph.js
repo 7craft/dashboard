9bbc6609b4f81b34882e3de1a5d4f0a23d0cc2d2
7craft <44245873+7craft@users.noreply.github.com>
2018-12-16 19:14:42 +0700

    Graph no css

--- src/js/Graph.js
+++ src/js/Graph.js
@@ -2,7 +2,7 @@ var chart_labels = ['06:00', '09:00', '12:00', '15:00', '18:00', '21:00'];
 var temp_dataset = ['20', '21', '25', '32', '30', '27'];
 var DO_dataset = ['6', '7', '8', '6', '4', '2'];
 
-var maxmin_Temp_Threshold = ['24','28']; // <------------------ Get FROM CLOUND /Setting / max
+var maxmin_Temp_Threshold = ['24','30']; // <------------------ Get FROM CLOUND /Setting / max
 var maxmin_DO_Threshold = ['4','15']; // <------------------ GET FROM CLOUND /setting / min
 
 //Texting
@@ -12,24 +12,52 @@ var toplabels_dataset = ['Temperature (Celsius)','Dissolved Oxygen[D.O.] (mg/L)'
 // styling value ----------------------
 
 var thresholdBroder = 3;
+//  COLOR  MAIN BLUE
+var maincolorTheme_BLUE = "rgba(80, 147, 225, 1)" ; // MAIN COLOUR 1 BLUE
+var maincolorTheme_BLUE_2 = "rgba(80, 147, 225, 0.8)" ; // MAIN COLOUR 1 BLUE
+
+//  COLOR  MAIN GREEN
+var maincolorTheme_GREEN = "rgba(80, 193, 78, 0.5)" ; // MAIN COLOUR 2 Green
+var maincolorTheme_GREEN_2 = "rgba(80, 193, 78, 0.8)" ; // MAIN COLOUR 2 Green
+
+//  COLOR  MAIN RED
+var maincolorTheme_RED = "rgba(246, 81, 119, 1)" ; // MAIN COLOUR 2 Green
+var maincolorTheme_RED_2 = "rgba(246, 81, 119, 0.7)" ; // MAIN COLOUR 2 Green
+
+var debug_red = "rgba(255, 0, 0, 1)";
 
 //----------------------------------
 var chart_labels2 = ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'];
-var temp_dataset2 = ['5', '3', '4', '8', '10', '11', '10', '9'];
-var rain_dataset2 = ['0', '0', '1', '4', '19', '19', '7', '2'];
-var ctx = document.getElementById("forecast").getContext('2d');
 
-var config = {
-    type: 'bar',
+
+
+// Config  FOR ALL 3 TYPES OF GRAPH
+// TEMP CHART
+var Temp_CHART_config = {
+    type: 'line',
     data: 
     {
         labels: chart_labels2,
         datasets: [
             {
                 type: 'line',
+                fill: false,
+
+                //Style of  the line
+                lineTension: 0.5 ,
+                borderColor: maincolorTheme_RED,
+                borderWidth: 2,
+
+                //background color
+                backgroundColor: maincolorTheme_RED,
+
+                // Style of the point
+                pointBackgroundColor: maincolorTheme_RED,
+                pointBorderColor: maincolorTheme_RED,
+                pointRadius:3,
+
                 label: toplabels_dataset[0],
                 yAxisID: "y-axis-0",
-                fill: false,
                 data: temp_dataset,
             },
         ]
@@ -42,6 +70,7 @@ var config = {
             yAxes: 
             [
                 {
+                    
                     position: "left",
                     "id": "y-axis-0",
                 },
@@ -59,16 +88,14 @@ var config = {
                     mode: 'horizontal',
                     scaleID: 'y-axis-0',
                     value: maxmin_Temp_Threshold[1],
-                    borderColor: '#f65177', //set color
+                    borderColor: maincolorTheme_RED_2, //set color
                     borderWidth: 4,
                     label: 
                     {
-                        backgroundColor: "#f65177",
+                        backgroundColor: maincolorTheme_RED_2,
                         content: "max",
                         enabled: true
-                    },
-                        
-                        
+                    },                     
                 },    
                 {
                     // Minmimum Threshold
@@ -77,33 +104,46 @@ var config = {
                     mode: 'horizontal',
                     scaleID: 'y-axis-0',
                     value: maxmin_Temp_Threshold[0],
-                    borderColor: '#f65177', //set color
+                    borderColor: maincolorTheme_RED_2, //set color
                     borderWidth: 4,
                     label: 
                     {
-                        backgroundColor: "#f65177",
+                        backgroundColor: maincolorTheme_RED_2,
                         content: "min",
                         enabled: true
                     },    
                 }
             ]
-          
         }
-
-
     }
 };
-var config2 = {
-    type: 'bar',
+// DO CHART -******************************************************************************************-
+var DO_CHART_config = {
+    type: 'line',
     data: {
         labels: chart_labels2,
         datasets: [
             {
                 type: 'line',
-                label: toplabels_dataset[0],
+                fill: false,
+
+                //Style of  the line
+                lineTension: 0.5 ,
+                borderColor: maincolorTheme_BLUE,
+                borderWidth: 2,
+
+                //background color
+                backgroundColor: maincolorTheme_BLUE,
+
+                // Style of the point
+                pointBackgroundColor: maincolorTheme_BLUE,
+                pointBorderColor: maincolorTheme_BLUE,
+                pointRadius:3,
+
+                label: toplabels_dataset[1],
                 yAxisID: "y-axis-0",
                 fill: false,
-                data: temp_dataset,
+                data: DO_dataset,
             },
         ]
     },
@@ -113,8 +153,8 @@ var config2 = {
             {
                 yAxes: [
                     {
-                    position: "left",
-                    "id": "y-axis-0",
+                        position: "left",
+                        "id": "y-axis-0",
                     },
                 ]
             },
@@ -128,11 +168,11 @@ var config2 = {
                         mode: 'horizontal',
                         scaleID: 'y-axis-0',
                         value: maxmin_DO_Threshold[1],
-                        borderColor: '#f65177', //set color
+                        borderColor: maincolorTheme_BLUE_2, //set color
                         borderWidth: 4,
                         label: 
                         {
-                            backgroundColor: "#f65177",
+                            backgroundColor: maincolorTheme_BLUE_2,
                             content: "max",
                             enabled: true
                         },
@@ -144,12 +184,12 @@ var config2 = {
                         drawTime: 'afterDatasetsDraw',
                         mode: 'horizontal',
                         scaleID: 'y-axis-0',
-                        value: maxmin_Temp_Threshold[0],
-                        borderColor: '#f65177', //set color
+                        value: maxmin_DO_Threshold[0],
+                        borderColor: maincolorTheme_BLUE_2, //set color
                         borderWidth: 4,
                         label: 
                         {
-                            backgroundColor: "#f65177",
+                            backgroundColor: maincolorTheme_BLUE_2,
                             content: "min",
                             enabled: true
                         },
@@ -161,80 +201,164 @@ var config2 = {
 
     }
 };
-var forecast_chart = new Chart(ctx, config);
-//console.log(forecast_chart.config.options.annoation)
-$("#0").click(function() {
-    var data = forecast_chart.config.data;
-    data.datasets[0].data = temp_dataset;
-    data.datasets[0].label = toplabels_dataset[0];
-    data.labels = chart_labels2;
-    //var annoation = forecast_chart.config.data;
-    forecast_chart.update();
-});
-$("#1").click(function() {
-    var data = forecast_chart.config.data;
-    data.datasets[0].data = DO_dataset;
-    data.datasets[0].label = toplabels_dataset[1];
-    data.labels = chart_labels2;
-    console.log(forecast_chart.config.options);
-    forecast_chart.config.options = 
-    
-    {
-    	responsive: true,
-        scales: 
-        {
-            yAxes: 
-            [
-                {
-                    position: "left",
-                    "id": "y-axis-0",
-                },
-            ]
-        },
-        annotation: 
-        {
-            drawTime: 'afterDatasetsDraw',
-            annotations: 
-            [
-                {
-                    // Maximum Threshold
-                    type: 'line',
-                    drawTime: 'afterDatasetsDraw',
-                    mode: 'horizontal',
-                    scaleID: 'y-axis-0',
-                    value: maxmin_DO_Threshold[1],
-                    borderColor: '#f65177', //set color
-                    borderWidth: 4,
-                    label: 
+// DO CHART -******************************************************************************************-
+var show_both_config = {
+    type: 'line',
+    data: {
+        labels: chart_labels2,
+        datasets: [
+            // Temp
+            {
+                type: 'line',
+                fill: false,
+
+                //Style of  the line
+                lineTension: 0.5 ,
+                borderColor: maincolorTheme_RED,
+                borderWidth: 2,
+
+                //background color
+                backgroundColor: maincolorTheme_RED,
+
+                // Style of the point
+                pointBackgroundColor: maincolorTheme_RED,
+                pointBorderColor: maincolorTheme_RED,
+                pointRadius:3,
+
+                label: toplabels_dataset[0],
+                yAxisID: "y-axis-0",
+                data: temp_dataset,
+            },
+            // DO 
+            {
+                type: 'line',
+                fill: false,
+
+                //Style of  the line
+                lineTension: 0.5 ,
+                borderColor: maincolorTheme_BLUE,
+                borderWidth: 2,
+
+                //background color
+                backgroundColor: maincolorTheme_BLUE,
+
+                // Style of the point
+                pointBackgroundColor: maincolorTheme_BLUE,
+                pointBorderColor: maincolorTheme_BLUE,
+                pointRadius:3,
+
+                label: toplabels_dataset[1],
+                yAxisID: "y-axis-0",
+                fill: false,
+                data: DO_dataset,
+            },
+        ]
+    },
+    options: {
+    		responsive: true,
+            scales: 
+            {
+                yAxes: [
                     {
-                        backgroundColor: "#f65177",
-                        content: "max",
-                        enabled: true
+                        position: "left",
+                        "id": "y-axis-0",
                     },
-                        
-                        
-                },    
-                {
-                    // Minmimum Threshold
-                    type: 'line',
-                    drawTime: 'afterDatasetsDraw',
-                    mode: 'horizontal',
-                    scaleID: 'y-axis-0',
-                    value: maxmin_DO_Threshold[0],
-                    borderColor: '#f65177', //set color
-                    borderWidth: 4,
-                    label: 
+                ]
+            },
+            annotation: {
+                drawTime: 'afterDatasetsDraw',
+                annotations: [
+                    //temp--------------------------------------------------------------------
                     {
-                        backgroundColor: "#f65177",
-                        content: "min",
-                        enabled: true
+                        // Maximum Threshold
+                        type: 'line',
+                        drawTime: 'afterDatasetsDraw',
+                        mode: 'horizontal',
+                        scaleID: 'y-axis-0',
+                        value: maxmin_Temp_Threshold[1],
+                        borderColor: maincolorTheme_RED_2, //set color
+                        borderWidth: 4,
+                        label: 
+                        {
+                            backgroundColor: maincolorTheme_RED_2,
+                            content: "max temp",
+                            enabled: true
+                        },                     
                     },    
-                }
-            ]
+                    {
+                        // SET DAGGER LINE ----------------------------------
+                        // Minmimum Threshold
+                        type: 'line',
+                        drawTime: 'afterDatasetsDraw',
+                        mode: 'horizontal',
+                        scaleID: 'y-axis-0',
+                        value: maxmin_Temp_Threshold[0],
+                        borderColor: maincolorTheme_RED_2, //set color
+                        borderWidth: 4,
+                        label: 
+                        {
+                            backgroundColor: maincolorTheme_RED_2,
+                            content: "min temp",
+                            enabled: true
+                        },    
+                    },
+                    // DO--------------------------------------------------------------------
+                    {
+                        // Maximum Threshold
+                        type: 'line',
+                        drawTime: 'afterDatasetsDraw',
+                        mode: 'horizontal',
+                        scaleID: 'y-axis-0',
+                        value: maxmin_DO_Threshold[1],
+                        borderColor: maincolorTheme_BLUE_2, //set color
+                        borderWidth: 4,
+                        label: 
+                        {
+                            backgroundColor: maincolorTheme_BLUE_2,
+                            content: "max d.o.",
+                            enabled: true
+                        },
+                        
+                    },
+                    {
+                        // Minmimum Threshold
+                        type: 'line',
+                        drawTime: 'afterDatasetsDraw',
+                        mode: 'horizontal',
+                        scaleID: 'y-axis-0',
+                        value: maxmin_DO_Threshold[0],
+                        borderColor: maincolorTheme_BLUE_2, //set color
+                        borderWidth: 4,
+                        label: 
+                        {
+                            backgroundColor: maincolorTheme_BLUE_2,
+                            content: "min d.o.",
+                            enabled: true
+                        },
+                    }
+                ]
           
-        }
+      }
 
 
     }
-    forecast_chart.update();
+};
+
+var ctx = document.getElementById("forecast").getContext('2d');
+var forecast_chart = new Chart(ctx, show_both_config);
+//console.log(forecast_chart.config.options.annoation)
+$("#0").click(function() {
+    forecast_chart.destroy();
+    forecast_chart = new Chart(ctx, show_both_config);
+    console.log("destroy D.O. CHART")
 });
+$("#1").click(function() {
+    forecast_chart.destroy();
+    forecast_chart = new Chart(ctx, Temp_CHART_config);
+});
+$("#2").click(function() {
+    forecast_chart.destroy();
+    forecast_chart = new Chart(ctx, DO_CHART_config);
+    console.log("destroy D.O. CHART")
+});
+
